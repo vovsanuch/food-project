37e46e83c681cf3523c9cadb86d4afadb376ba9e
Полещук Владимир Александрович <v.poleshuk@kifato.local>
2022-08-29 21:22:57 +0300

    gh-pages added

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import {BrowserRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter basename='/react-food'>
+    <BrowserRouter basename='/food-project'>
         <App />
     </BrowserRouter>
 );
